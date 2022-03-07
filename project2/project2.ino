9e66da584a211ad8ca351904e5a89e2fed94faf6
heavysugar <44726789+heavysugar@users.noreply.github.com>
2022-03-07 15:48:43 -0500

    added lines that read potentiometer and button at the same time with delay 500

--- project2/project2.ino
+++ project2/project2.ino
@@ -3,38 +3,15 @@ This is an example how to use Touch Intrrerupts and read touch values
 */
 
 int threshold = 40;
-bool touch1detected = false;
-bool touch2detected = false;
-uint8_t touch1Val = 0;
-uint8_t touch2Val = 0;
-
-void gotTouch1(){
- touch1detected = true;
- touch1Val = touchRead(T2);
-}
-
-void gotTouch2(){
- touch2detected = true;
- touch2Val = touchRead(T9);
-}
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); // give me time to bring up serial monitor
-  Serial.println("ESP32 Touch Interrupt Test");
-  touchAttachInterrupt(T2, gotTouch1, threshold);
-  touchAttachInterrupt(T9, gotTouch2, threshold);
   pinMode(13, INPUT_PULLUP);
 }
 
 void loop(){
   Serial.println(digitalRead(13));
-//  if(touch1detected){
-//    touch1detected = false;
-//    Serial.println(touch1Val);
-//  }
-//  if(touch2detected){
-//    touch2detected = false;
-//    Serial.println(touch2Val);
-//  }
+  delay(500); 
+  Serial.println(analogRead(12));
+ 
 }
