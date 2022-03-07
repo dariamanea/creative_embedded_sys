63db9638dac1f94fd060e83de7b6fadc80305cf3
heavysugar <44726789+heavysugar@users.noreply.github.com>
2022-03-07 16:13:51 -0500

    added joystick reader

--- project2/project2.ino
+++ project2/project2.ino
@@ -6,12 +6,33 @@ int threshold = 40;
 
 void setup() {
   Serial.begin(115200);
+  // button
   pinMode(13, INPUT_PULLUP);
+  // SW from joystick 
+  pinMode(25, INPUT_PULLUP);
+
 }
 
+
 void loop(){
+  // button
   Serial.println(digitalRead(13));
   delay(500); 
+  // potentiometer 
   Serial.println(analogRead(12));
+  delay(500); 
+  // SW
+  Serial.println(digitalRead(25));
+  delay(500); 
+   // potentiometer VRY 
+  Serial.println(analogRead(27));
+  delay(500); 
+   // potentiometer VRX
+  Serial.println(analogRead(26));
+  delay(500); 
+
+
+
+
  
 }
