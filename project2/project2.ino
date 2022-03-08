1d7a5b9be654713374cb100633760871ea27ac78
heavysugar <44726789+heavysugar@users.noreply.github.com>
2022-03-07 23:06:34 -0500

    send messages without JSON

--- project2/project2.ino
+++ project2/project2.ino
@@ -8,8 +8,8 @@ int threshold = 40;
 DynamicJsonDocument doc(1024);
 
 void setup() {
-//  Serial.begin(115200);
-  Serial.begin(9600);
+  Serial.begin(115200);
+//  Serial.begin(9600);
   // button
   pinMode(13, INPUT_PULLUP);
   // SW from joystick 
@@ -21,10 +21,33 @@ void setup() {
 void loop(){
   
 //  doc["button"] = digitalRead(13);
-  doc["potentiometer"]   = analogRead(12);
-  doc["vrx"][0] = analogRead(26);
-  doc["vry"][1] = analogRead(27);
-  doc["sw"][1] = digitalRead(25);
-  serializeJson(doc, Serial);
-  delay(1); 
+//  doc["potentiometer"]   = analogRead(12);
+//  doc["vrx"][0] = analogRead(26);
+//  doc["vry"][1] = analogRead(27);
+//  doc["sw"][1] = digitalRead(25);
+//  serializeJson(doc, Serial);
+
+   Serial.println("S");
+
+  // button
+//  Serial.print("Button ");
+//  Serial.println(digitalRead(13));
+//
+  delay(10); 
+  
+  // potentiometer 
+ Serial.println(analogRead(12));
+  delay(10); 
+  
+  // SW
+  Serial.println(digitalRead(25));
+  delay(10); 
+  
+   // potentiometer VRY 
+  Serial.println(analogRead(27));
+  delay(500); 
+  
+//    potentiometer VRX
+  Serial.println(analogRead(26));
+  delay(500); 
 }
--- project2/script.js
+++ project2/script.js
@@ -1,6 +1,35 @@
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
+function fetchStream() {
+    const reader = stream.getReader();
+    let charsReceived = 0;
+  
+    // read() returns a promise that resolves
+    // when a value has been received
+    reader.read().then(function processText({ done, value }) {
+      // Result objects contain two properties:
+      // done  - true if the stream has already given you all its data.
+      // value - some data. Always undefined when done is true.
+      if (done) {
+        console.log("Stream complete");
+        para.textContent = value;
+        return;
+      }
+  
+      // value for fetch streams is a Uint8Array
+      charsReceived += value.length;
+      const chunk = value;
+      let listItem = document.createElement('li');
+      listItem.textContent = 'Received ' + charsReceived + ' characters so far. Current chunk = ' + chunk;
+      list2.appendChild(listItem);
+  
+      result += chunk;
+  
+      // Read some more, and call this function again
+      return reader.read().then(processText);
+    });
+  }
 
 if ("serial" in navigator) {
     // The Web Serial API is supported.
@@ -9,32 +38,61 @@ if ("serial" in navigator) {
         const port = await navigator.serial.requestPort();
         // Wait for the serial port to open.
         await port.open({
-            baudRate: 9600
+            baudRate: 115200,
+            bufferSize : 2048
         });
-        const textDecoder = new TextDecoderStream();
-        const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-        const reader = textDecoder.readable.getReader();
-        // const reader = textDecoder.readable.pipeThrough(new TransformStream(new LineBreakTransformer())).getReader();
-        
-        // Listen to data coming from the serial device.
-        while (true) {
-          const { value, done } = await reader.read();
-          if (done) {
-            // Allow the serial port to be closed later.
-            reader.releaseLock();
-            break;
-          }
-          // value is a string.
+
+        // const textDecoder = new TextDecoderStream();
+        // const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+
+        // while (port.readable) {
+        //     const reader = textDecoder.readable.getReader();
           
-        //   parsedVal = parseInt(value);
-          console.log(value)
-        //   if (!isNaN(parsedVal)) {
-        //     console.log(parsedVal)
-        //     // counterVal += parseInt(value);
-        //     //add a tiny bit extra to the time to make Tone.js happy
-        //     // synth.triggerAttackRelease((counterVal % 800) + 200, "8n", Tone.now()+counterVal/8000);
+        //     try {
+        //       while (true) {
+        //         const { value, done } = await reader.read();
+        //         if (done) {
+        //           // Allow the serial port to be closed later.
+        //           reader.releaseLock();
+        //           break;
+        //         }
+        //         if (value) {
+        //           console.log(value);
+        //         }
+        //       }
+        //     } catch (error) {
+        //       // TODO: Handle non-fatal read error.
+        //     }
         //   }
-        }
+        // With transform streams.
+
+const textDecoder = new TextDecoderStream();
+const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+const reader = textDecoder.readable.getReader();
+
+// Listen to data coming from the serial device.
+while (true) {
+  const { value, done } = await reader.read();
+  if (done) {
+    reader.releaseLock();
+    break;
+  }
+  // value is a string.
+  console.log(value);
+}
+
+const textEncoder = new TextEncoderStream();
+const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+reader.cancel();
+await readableStreamClosed.catch(() => { /* Ignore the error */ });
+
+writer.close();
+await writableStreamClosed;
+
+await port.close();
+
+       
     });
 
 }
